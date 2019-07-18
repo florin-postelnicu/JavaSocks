b77c1fcc89bd65df5e52679230b27ae3b64e1daf
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-07-18 12:08:27 -0700

    Add files via upload

---
+++ JavaClientServer/JavaServer.java
@@ -0,0 +1,26 @@
+
+
+
+import java.net.*;
+import java.io.*;
+
+
+public class JavaServer {
+    public static void main(String[] args) throws IOException{
+        ServerSocket ss =new ServerSocket(4999);
+        Socket s = ss.accept();
+
+        System.out.println("client connected");
+
+        InputStreamReader in = new InputStreamReader((s.getInputStream()));
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("client : "+ str);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("Yes");
+        pr.flush();
+
+    }
+}
